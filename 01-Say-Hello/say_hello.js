fd44c7d2fbd321a31779284f81ffbb24e1cbc4e2
zamanbhutta <marham.ayat@gmail.com>
2021-02-19 12:28:44 +0100

    2nd day exercise 1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,15 @@
 //insert your pseudocode below
 /*
-
-here
-
+create input of your name
+prompt text with massage your name
+create text with massage and your name
+print the massage
 */
 
 //insert your code below
+var Name, Massage;
+
+
+Name = window.prompt('Name');
+Massage = 'Hi ' + String(Name);
+window.alert(Massage);
