1cd34a970c2dbd72a645482290d383c3f139a38a
zamanbhutta <marham.ayat@gmail.com>
2021-02-19 14:45:12 +0100

    exercise 2

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,14 @@
 //insert your pseudocode below
 /*
-
-here
-
+create variable with input User text
+prompt this variable with txt msg, enter the text
+print the user text and count character
 */
 
 //insert your code below
+var user_text, count_character;
+
+
+user_text = window.prompt('enter the  text');
+count_character = user_text.length;
+window.alert([user_text,'   no of character is ',count_character].join(''));
